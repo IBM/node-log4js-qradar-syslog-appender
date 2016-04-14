2218fa22ffae799cd655018406a6046c298049fb
hermanba <hermanba@ca.ibm.com>
2016-04-14 01:11:42 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,104 @@
+/**
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2016. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ */
+'use strict';
+
+var log4js = require('log4js'),
+    syslogConnectionSingleton = require('./syslog-connection-singleton'),
+    tls = require('tls'),
+    fs = require('fs'),
+    _ = require('underscore'),
+    util = require('util');
+
+
+// Configuration for logging
+// log4js.configure("./config/log4js.json", {
+//     reloadSecs: 30
+// });
+
+// var logger = log4js.getLogger('qradar-syslog-appender-lib');
+
+
+
+module.exports = {
+    appender: appender,
+    configure: configure
+}
+
+function appender(options) {
+    return function(log) {
+        if (!syslogConnectionSingleton.connection) {
+            fs.readFile(options.certificatePath, function(err, certificate) {
+                if (err) {
+                    console.error('Error while loading certificate from path: ' + options.certificatePath + ' Error: ' + JSON.stringify(err, null, 2));
+                    return;
+                }
+
+                fs.readFile(options.privateKeyPath, function(err, key) {
+                    if (err) {
+                        console.error('Error while loading private key from path: ' + options.privateKeyPath + ' Error: ' + JSON.stringify(err, null, 2));
+                        return;
+                    }
+
+                    var tlsOptions = _.omit(options, ['certificatePath', 'privateKeyPath']);
+
+                    tlsOptions = _.extend(tlsOptions, {
+                        cert: certificate,
+                        key: key
+                    });
+
+                    syslogConnectionSingleton.connection = tls.connect(tlsOptions, logMessage.bind(this, log));
+                    syslogConnectionSingleton.connection.setEncoding('utf8');
+                    syslogConnectionSingleton.connection.on('error', function(err) {
+                        console.error('error in connection. Error: ' + JSON.stringify(err, null, 2));
+                    });
+                    syslogConnectionSingleton.connection.on('close', function(err) {
+                        console.warn('Connection closed. Error: ' + JSON.stringify(err, null, 2));
+                    });
+                    syslogConnectionSingleton.connection.on('end', function(err) {
+                        console.warn('Connection ended. Error: ' + JSON.stringify(err, null, 2));
+                    });
+
+                });
+            });
+        } else {
+            logMessage(log);
+        }
+    }
+};
+
+function logMessage(log) {
+    console.log('category: ' + log.categoryName);
+    if (log.categoryName !== 'audit-logs') return;
+
+    var message = log.data.join(' | ');
+    syslogConnectionSingleton.connection.write(new Buffer(message, 'utf8'));
+    console.log('message: ' + message);
+}
+
+function formattedMessage(message) {
+    return util.format(
+        '<%d>%s %s %s[%d]:%s\n',
+        '1',
+        new Date().toJSON(),
+        'localhost',
+        'otc-api',
+        process.pid,
+        message || 'Missing message'
+    );
+}
+
+function configure(config) {
+
+    if (config.appender) {
+        log4js.loadAppender(config.appender.type);
+        config.actualAppender = log4js.appenderMakers[config.appender.type](config.appender);
+    }
+
+    return appender(config.options);
+};
---
+++ syslog-connection-singleton.js
@@ -0,0 +1,29 @@
+/**
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2016. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ */
+'use strict';
+
+// internal constructor only
+var singleton = function() {
+    return {
+        connection : null
+    };
+};
+
+// returns the current instance if it exists
+// otherwise creates a new one.
+singleton.getInstance = function() {
+    if (!this.instance) {
+        this.instance = new singleton();
+    }
+    return this.instance;
+};
+
+// only expose a function that returns the current instance if it exists
+// otherwise creates a new one.
+module.exports = singleton.getInstance;
